bef9edc7525c8b24817f250bf8f8efe8eee8a006
JuYinC <juyinchao@gmail.com>
2025-12-06 15:23:31 +0800

    test

--- backend/src/main/java/com/finance/manager/FinanceManagerApplication.java
+++ backend/src/main/java/com/finance/manager/FinanceManagerApplication.java
@@ -8,7 +8,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 public class FinanceManagerApplication {
 
-
     public static void main(String[] args) {
         SpringApplication.run(FinanceManagerApplication.class, args);
     }
